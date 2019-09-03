e7d5030aea5a820e5fb6f96018b404c2b605221b
Nhan Ho <nhanhocumc@gmail.com>
2019-09-03 06:20:02 +0000

    version 1.1

---
+++ R/gtab.3stud.R
@@ -0,0 +1,30 @@
+#' Test datasets for microbiome age prediction.
+#'
+#' A list of three datasets to be used as test datasets
+#' for microbiome age prediction using Random Forest model.
+#'
+#' @docType data
+#'
+#' @usage data(gtab.3stud)
+#'
+#' @format A list of three dataframes.
+#'
+#' @keywords test datasets
+#'
+#' @references Subramanian et al. Nature. 2014 Jun 19; 510(7505): 417–421.
+#' (\href{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4189846/}{PubMed})
+#'
+#' Bender et al. Sci Transl Med. 2016 Jul 27; 8(349): 349ra100.
+#' (\href{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC5301310/}{PubMed})
+#'
+#' Pannaraj et al. JAMA Pediatr. 2017;90095(7):647–54.
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/28492938}{PubMed})
+#'
+#' Thompson et al. Front Cell Infect Microbiol. 2015;5:3.
+#' (\href{https://www.ncbi.nlm.nih.gov/pubmed/25705611?dopt=Abstract}{PubMed})
+#'
+#' @source \href{https://gordonlab.wustl.edu/Subramanian_6_14/Nature_2014_Processed_16S_rRNA_datasets.html}{Gordon Lab}
+#'
+#' @examples
+#' data(gtab.3stud)
+"gtab.3stud"
---
+++ R/taxtab6.R
@@ -0,0 +1,27 @@
+#' Taxonomic relative abundance data.
+#'
+#' Monthly longitudinal relative abundance data from phylum to genus level of
+#' 50 infants from birth to 2 year of life.
+#' Mapping file is merged to the data for ready use.
+#'
+#' @docType data
+#'
+#' @usage data(taxtab6)
+#'
+#' @format A data frame with 322 row (samples) and 803 variables (including mapping varilable and bacterial taxonomies from phylum to genus level).
+#'
+#' @keywords datasets
+#'
+#' @references Subramanian et al. Nature. 2014 Jun 19; 510(7505): 417–421.
+#' (\href{https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4189846/}{PubMed})
+#'
+#' @source \href{https://gordonlab.wustl.edu/Subramanian_6_14/Nature_2014_Processed_16S_rRNA_datasets.html}{Gordon Lab}
+#'
+#' @examples
+#' #Load summary tables of bacterial taxa relative abundance from Bangladesh data
+#' data(taxtab6)
+#' tl<-colnames(taxtab6)[grep("k__bacteria.p__fusobacteria",colnames(taxtab6))]
+#' taxacom.ex<-taxa.compare(taxtab=taxtab6[,c("personid","x.sampleid","bf","age.sample",tl)],
+#' propmed.rel="gamlss",comvar="bf",adjustvar="age.sample",
+#' longitudinal="yes",p.adjust.method="fdr")
+"taxtab6"
